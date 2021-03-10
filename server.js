43970aa9b273d53d9e43f69bf34d127f080965e4
NerdTechApp <57812206+NerdTechApp@users.noreply.github.com>
2021-03-10 12:01:03 -0300

    server.js
    Update server json

--- server.js
+++ server.js
@@ -1,7 +1,323 @@
 {
-  "Avatares" : {
-    "Categoria1" : {
-      "01" : "01\\"
+  "ADM" : {
+    "yTuiEYIv3PZSTj4Qjr4pR40yERn1" : {
+      "email" : "cpgospels2017@gmail.com",
+      "uid" : "yTuiEYIv3PZSTj4Qjr4pR40yERn1"
+    }
+  },
+  "Data" : {
+    "App" : {
+      "Manutenção" : {
+        "status" : {
+          "status" : "aberto"
+        }
+      }
+    }
+  },
+  "Filmes" : {
+    " A Vida Extraordinária de David Copperfield" : {
+      "ano" : "2021",
+      "categoria" : "DRAMA",
+      "descricao" : "David Copperfield é um jovem homem de baixa classe que, deixado órfão, precisa navegar um mundo caótico em busca de sua identidade e seu lugar. Entre problemas de classe e as dificuldades da vida adulta, ele descobre um exímio talento para contar histórias e encontra o amor.",
+      "episodios" : "[{\"temporadaE\":\"Assistir\",\"link\":\"https://tgfilestreamer.herokuapp.com/14973598590386356/@BluRayFilmes_A_Vida_Extraordin%C3%A1ria_de_David_Copperfield_2021.mp4\",\"episodioE\":\"DUBLADO\"}]",
+      "filtro" : "todos",
+      "foto" : "https://firebasestorage.googleapis.com/v0/b/streamingfast-d7721.appspot.com/o/storage%2FIMG_20210204_104657_444.jpg?alt=media&token=054f8a68-f8b3-4531-9a7d-00ec053271e6",
+      "imdb" : "6.0",
+      "nome" : " A Vida Extraordinária de David Copperfield",
+      "temporadas" : "[{\"temporadaT\":\"Assistir\"}]",
+      "tipo" : "FILME",
+      "trailer" : "n/a"
+    },
+    "A Assistente" : {
+      "ano" : "2020",
+      "categoria" : "DRAMA",
+      "descricao" : "Jane (Julia Garner) é uma aspirante a produtora de cinema que recentemente conseguiu seu emprego dos sonhos como assistente júnior de um poderoso magnata do entretenimento. O dia dela é muito parecido com o de qualquer outra assistente. Mas, à medida que Jane segue sua rotina diária, ela começa a perceber todos os abusos que envolvem seu ambiente de trabalho e sua poisção profissional.",
+      "episodios" : "[{\"temporadaE\":\"Assistir\",\"link\":\"https://tgfilestreamer.herokuapp.com/14844646492291252/A%20Assistente%20%5B2020%5D%20720p%20BluRay%20Dub%20@PFilmes.mp4\",\"episodioE\":\"Dublado\"}]",
+      "filtro" : "todos",
+      "foto" : "https://firebasestorage.googleapis.com/v0/b/streamingfast-d7721.appspot.com/o/storage%2FIMG_20210201_134628_120.jpg?alt=media&token=0c35ce99-4b86-4ba7-a209-ce1e044d53a3",
+      "imdb" : "7.0",
+      "nome" : "A Assistente",
+      "temporadas" : "[{\"temporadaT\":\"Assistir\"}]",
+      "tipo" : "FILME",
+      "trailer" : "n/a"
+    },
+    "A Escavação" : {
+      "ano" : "2021",
+      "categoria" : "DRAMA",
+      "descricao" : "Às vésperas da Segunda Guerra Mundial, uma rica viúva contrata um arqueólogo amador para escavar sepultamentos ancestrais em sua propriedade. Ao fazerem uma descoberta histórica, os ecos do passado ressoam em um futuro de incertezas.",
+      "episodios" : "[{\"temporadaE\":\"Assistir\",\"link\":\"https://tgfilestreamer.herokuapp.com/15042043189215412/@BluRayFilmes_A%20Escava%C3%A7%C3%A3o%20(2021\",\"episodioE\":\"Dublado\"}]",
+      "filtro" : "todos",
+      "foto" : "https://firebasestorage.googleapis.com/v0/b/streamingfast-d7721.appspot.com/o/storage%2FIMG_20210205_174729_921.jpg?alt=media&token=bf092bbf-e9a8-4de1-b92c-3f8969a5c133",
+      "imdb" : "7.0",
+      "nome" : "A Escavação",
+      "temporadas" : "[{\"temporadaT\":\"Assistir\"}]",
+      "tipo" : "FILME",
+      "trailer" : "n/a"
+    },
+    "A Lista de Schindler" : {
+      "ano" : "1993",
+      "categoria" : "DRAMA",
+      "descricao" : "O alemão Oskar Schindler viu na mão de obra judia uma solução barata e viável para lucrar com negócios durante a guerra. Com sua forte influência dentro do partido nazista, foi fácil conseguir as autorizações e abrir uma fábrica. O que poderia parecer uma atitude de um homem não muito bondoso, transformou-se em um dos maiores casos de amor à vida da História, pois este alemão abdicou de toda sua fortuna para salvar a vida de mais de mil judeus em plena luta contra o extermínio alemão.",
+      "episodios" : "[]",
+      "filtro" : "todos",
+      "foto" : "https://firebasestorage.googleapis.com/v0/b/streamingfast-d7721.appspot.com/o/storage%2FIMG_20210204_105535_305.jpg?alt=media&token=5810ce64-d6b9-4086-97bb-e392e56df952",
+      "imdb" : "8.0",
+      "nome" : "A Lista de Schindler",
+      "temporadas" : "[]",
+      "tipo" : "FILME",
+      "trailer" : "n/a"
+    },
+    "Aquaman" : {
+      "ano" : "2019",
+      "categoria" : "AÇÃO",
+      "descricao" : "A cidade de Atlantis, que já foi lar de uma das mais avançadas civilizações do mundo, agora é um reino submerso dominado pelo ganancioso Rei Orm. Com um vasto exército a seu dispor, Orm planeja conquistar tanto os demais reinos do oceano, quanto o mundo da superfície. Em seu caminho está Aquaman, meio-humano e meio-atlante, e verdadeiro herdeiro do trono. Com a ajuda da princesa Mera, Aquaman precisa recuperar o lendário Tridente de Atlan e aceitar seu destino como protetor das profundezas.",
+      "episodios" : "[{\"temporadaE\":\"Assistir\",\"link\":\"https://tgfilestreamer.herokuapp.com/15042129088561332/@BluRayFilmes_Aquaman%20(2019\",\"episodioE\":\"Dublado\"}]",
+      "filtro" : "todos",
+      "foto" : "https://firebasestorage.googleapis.com/v0/b/streamingfast-d7721.appspot.com/o/storage%2FIMG_20210205_175223_452.jpg?alt=media&token=e40286d6-3b11-49f4-89ff-9bb4ad7034cc",
+      "imdb" : "6.3",
+      "nome" : "Aquaman",
+      "temporadas" : "[{\"temporadaT\":\"Assistir\"}]",
+      "tipo" : "FILME",
+      "trailer" : "n/a"
+    },
+    "Capitão América 2: O Soldado Invernal" : {
+      "ano" : "2014",
+      "categoria" : "AÇÃO",
+      "descricao" : "Após os eventos catastróficos em Nova York com Os Vingadores, Steve Rogers, segue tentando se ajustar ao mundo moderno. Porém, quando um colega da agência S.H.I.E.L.D. é atacado, Steve se vê preso em uma rede de intrigas que ameaça colocar o mundo em risco. Em parceria com a Viúva Negra e Falcão, seu novo aliado, o Capitão América tem que enfrentar um misterioso e inesperado inimigo, o Soldado Invernal.",
+      "episodios" : "[{\"temporadaE\":\"Assistir\",\"link\":\"https://tgfilestreamer.herokuapp.com/14972705237188788/@BluRayFilmes_Capit%C3%A3o%20Am%C3%A9rica%202%20O%20Soldado%20Invernal%20(2014\",\"episodioE\":\"Dublado\"}]",
+      "filtro" : "todos",
+      "foto" : "https://firebasestorage.googleapis.com/v0/b/streamingfast-d7721.appspot.com/o/storage%2FIMG_20210204_101035_621.jpg?alt=media&token=d4813262-2204-454b-b0b6-a4755aaa6b13",
+      "imdb" : "8.0",
+      "nome" : "Capitão América 2: O Soldado Invernal",
+      "temporadas" : "[{\"temporadaT\":\"Assistir\"}]",
+      "tipo" : "FILME",
+      "trailer" : "n/a"
+    },
+    "Capitão América: Guerra Civil" : {
+      "ano" : "2016",
+      "categoria" : "AÇÃO",
+      "descricao" : "O ataque de Ultron fez com que os políticos buscassem algum meio de controlar os super-heróis, já que seus atos afetam toda a humanidade. Tal decisão coloca o Capitão América em rota de colisão com Tony Stark, o Homem de Ferro.",
+      "episodios" : "[{\"temporadaE\":\"Assistir\",\"link\":\"https://tgfilestreamer.herokuapp.com/14972911395618996/@BluRayFilmes_Capit%C3%A3o%20Am%C3%A9rica%20Guerra%20Civil%20(2016\",\"episodioE\":\"Dublado\"}]",
+      "filtro" : "todos",
+      "foto" : "https://firebasestorage.googleapis.com/v0/b/streamingfast-d7721.appspot.com/o/storage%2FIMG_20210204_101323_256.jpg?alt=media&token=90a599fc-9f5c-4f5c-a9ba-9920efb57fb8",
+      "imdb" : "8.0",
+      "nome" : "Capitão América: Guerra Civil",
+      "temporadas" : "[{\"temporadaT\":\"Assistir\"}]",
+      "tipo" : "FILME",
+      "trailer" : "n/a"
+    },
+    "Invasão Zumbi" : {
+      "ano" : "2016",
+      "categoria" : "AÇÃO",
+      "descricao" : "Em um trem de alta velocidade com destino à cidade de Busan, um vírus que transforma as pessoas em zumbis, se espalha. A cidade conseguiu com sucesso defender a epidemia, agora eles devem lutar pelas suas sobrevivências.",
+      "episodios" : "[]",
+      "filtro" : "todos",
+      "foto" : "https://firebasestorage.googleapis.com/v0/b/streamingfast-d7721.appspot.com/o/storage%2FIMG_20210202_220735_245.jpg?alt=media&token=c7985430-0e77-4823-a4a7-2599a56f3e06",
+      "imdb" : "8.0",
+      "nome" : "Invasão Zumbi",
+      "temporadas" : "[]",
+      "tipo" : "FILME",
+      "trailer" : "n/a"
+    },
+    "Invocação do Mal" : {
+      "ano" : "2013",
+      "categoria" : "TERROR",
+      "descricao" : "Os investigadores paranormais Ed e Lorraine Warren trabalham para ajudar a família aterrorizada por uma entidade demoníaca em sua fazenda.",
+      "episodios" : "[{\"temporadaE\":\"Assistir\",\"link\":\"https://tgfilestreamer.herokuapp.com/14833479577321652/@BluRayFilmes_Invoca%C3%A7%C3%A3o%20do%20Mal%20(2013\",\"episodioE\":\"Dublado\"}]",
+      "filtro" : "todos",
+      "foto" : "https://firebasestorage.googleapis.com/v0/b/streamingfast-d7721.appspot.com/o/storage%2FIMG_20210201_081307_413.jpg?alt=media&token=7bfc3e21-dca4-4719-bad3-b20a56d6c193",
+      "imdb" : "7.5",
+      "nome" : "Invocação do Mal",
+      "temporadas" : "[{\"temporadaT\":\"Assistir\"}]",
+      "tipo" : "FILME",
+      "trailer" : "n/a"
+    },
+    "Invocação do Mal 2" : {
+      "ano" : "2016",
+      "categoria" : "TERROR",
+      "descricao" : "Os famosos demonologistas Lorraine e Ed Warren viajam até o norte de Londres. Lá, eles ajudam uma mãe solteira que cria quatro filhos sozinha em uma casa atormentada por espíritos malignos.",
+      "episodios" : "[{\"temporadaE\":\"Assistir\",\"link\":\"https://tgfilestreamer.herokuapp.com/14836331435606196/@BluRayFilmes_Invoca%C3%A7%C3%A3o%20do%20Mal%202%20(2016\",\"episodioE\":\"Dublado\"}]",
+      "filtro" : "todos",
+      "foto" : "https://firebasestorage.googleapis.com/v0/b/streamingfast-d7721.appspot.com/o/storage%2FIMG_20210201_104128_854.jpg?alt=media&token=c175ec9f-38ac-4c6a-8039-e07b11721c51",
+      "imdb" : "7.2",
+      "nome" : "Invocação do Mal 2",
+      "temporadas" : "[{\"temporadaT\":\"Assistir\"}]",
+      "tipo" : "FILME",
+      "trailer" : "n/a"
+    },
+    "Isolado na Pandemia" : {
+      "ano" : "2020",
+      "categoria" : "TERROR",
+      "descricao" : "Após a erupção de uma pandemia global, um homem barricado sozinho em casa e racionando comida começa a tentar resgatar uma vizinha depois de uma enxame de hordas de Screamers mortos-vivos invadir seu complexo de apartamentos.",
+      "episodios" : "[{\"temporadaE\":\"Assistir\",\"link\":\"https://tgfilestreamer.herokuapp.com/14844543413076148/Isolado%20na%20Pandemia%20%5B2021%5D%20720p%20BluRay%20Dub%20@PFilmes.mp4\",\"episodioE\":\"Dublado\"}]",
+      "filtro" : "todos",
+      "foto" : "https://firebasestorage.googleapis.com/v0/b/streamingfast-d7721.appspot.com/o/storage%2FIMG_20210201_133835_590.jpg?alt=media&token=61d5b8a7-2535-401c-90f3-86f59c83426e",
+      "imdb" : "7.2",
+      "nome" : "Isolado na Pandemia",
+      "temporadas" : "[{\"temporadaT\":\"Assistir\"}]",
+      "tipo" : "FILME",
+      "trailer" : "n/a"
+    },
+    "Mulher Maravilha 1984" : {
+      "ano" : "2021",
+      "categoria" : "AÇÃO",
+      "descricao" : "Em 1984, Diana Prince entra em conflito com dois inimigos formidáveis, Maxwell Lord e a Mulher-Leopardo, enquanto reencontra misteriosamente com seu antigo interesse amoroso Steve Trevor.",
+      "episodios" : "[]",
+      "filtro" : "todos",
+      "foto" : "https://firebasestorage.googleapis.com/v0/b/streamingfast-d7721.appspot.com/o/storage%2FIMG_20210204_105121_736.jpg?alt=media&token=1fcf24d2-744f-484d-b030-b8566f46e394",
+      "imdb" : "6.0",
+      "nome" : "Mulher Maravilha 1984",
+      "temporadas" : "[]",
+      "tipo" : "FILME",
+      "trailer" : "n/a"
+    },
+    "Os Escolhidos" : {
+      "ano" : "2013",
+      "categoria" : "TERROR",
+      "descricao" : "Uma sequência de acontecimentos estranhos passa a atormentar a família Barrett, que mora em uma cidade tranquila nos Estados Unidos. Lacy, a mãe, busca explicações para os bizarros fenômenos que perturbam seu lar.",
+      "episodios" : "[{\"temporadaE\":\"Assistir\",\"link\":\"https://tgfilestreamer.herokuapp.com/14973564230647988/@BluRayFilmes_Os%20Escolhidos%20(2013\",\"episodioE\":\"Dublado\"}]",
+      "filtro" : "todos",
+      "foto" : "https://firebasestorage.googleapis.com/v0/b/streamingfast-d7721.appspot.com/o/storage%2FIMG_20210204_104352_745.jpg?alt=media&token=b36700fb-d1d9-490c-944e-f31ccffc2360",
+      "imdb" : "6.3",
+      "nome" : "Os Escolhidos",
+      "temporadas" : "[{\"temporadaT\":\"Assistir\"}]",
+      "tipo" : "FILME",
+      "trailer" : "n/a"
+    },
+    "PALMER" : {
+      "ano" : "2021",
+      "categoria" : "DRAMA",
+      "descricao" : "Após 12 anos na prisão, Eddie Palmer volta para casa para reconstruir sua vida. Ele cria uma ligação inesperada com Sam, um rapaz marginalizado que vem de uma família problemática e, agora, o passado de Eddie ameaça arruinar a sua nova vida.",
+      "episodios" : "[{\"temporadaE\":\"Assistir\",\"link\":\"https://tgfilestreamer.herokuapp.com/14834544729211060/Palmer%20%5B2021%5D%20720p%20Web-dl%20Dub%20@PFilmes.mp4\",\"episodioE\":\"Dual áudio\"}]",
+      "filtro" : "todos",
+      "foto" : "https://firebasestorage.googleapis.com/v0/b/streamingfast-d7721.appspot.com/o/storage%2FIMG_20210201_092040_395.jpg?alt=media&token=0c876055-1785-4378-ae74-3c1409b9c10e",
+      "imdb" : "7.2",
+      "nome" : "PALMER",
+      "temporadas" : "[{\"temporadaT\":\"Assistir\"}]",
+      "tipo" : "FILME",
+      "trailer" : "n/a"
+    },
+    "Pai em Dobro" : {
+      "ano" : "2021",
+      "categoria" : "COMÉDIA",
+      "descricao" : "Depois de passar a vida toda numa comunidade hippie, uma garota de 18 anos aproveita a chance de sair para o mundo real e decide procurar o pai.",
+      "episodios" : "[{\"temporadaE\":\"Assistir\",\"link\":\"https://tgfilestreamer.herokuapp.com/14975522735734964/@BluRayFilmes_Pai%20em%20Dobro%20(2021\",\"episodioE\":\"DUBLADO\"}]",
+      "filtro" : "todos",
+      "foto" : "https://firebasestorage.googleapis.com/v0/b/streamingfast-d7721.appspot.com/o/storage%2FIMG_20210204_114324_532.jpg?alt=media&token=93090612-90c3-429a-a171-9204a38a1bde",
+      "imdb" : "6.0",
+      "nome" : "Pai em Dobro",
+      "temporadas" : "[{\"temporadaT\":\"Assistir\"}]",
+      "tipo" : "FILME",
+      "trailer" : "n/a"
+    },
+    "Seremos História?" : {
+      "ano" : "2016",
+      "categoria" : "DOCUMENTÁRIO",
+      "descricao" : "Um problema de longas datas está se mostrando cada vez mais grave. Mudanças de temperatura, inundações, e outros fenômenos semelhantes têm se tornado cada vez mais comum, e o que tem sido feito na tentativa de amenizar essa situação? É na busca por respostas para essas questões que Leonardo DiCaprio abraça a missão de testemunhar as mudanças climáticas em diversos países, e de uma forma nunca vista antes.",
+      "episodios" : "[{\"temporadaE\":\"Assistir\",\"link\":\"https://tgfilestreamer.herokuapp.com/15042008829477044/@BluRayFilmes_Seremos%20Hist%C3%B3ria%3F%20(2016\",\"episodioE\":\"Dublado\"}]",
+      "filtro" : "todos",
+      "foto" : "https://firebasestorage.googleapis.com/v0/b/streamingfast-d7721.appspot.com/o/storage%2FIMG_20210205_174334_432.jpg?alt=media&token=b2051bd9-c8be-4c91-a45d-9174ead906b7",
+      "imdb" : "8.3",
+      "nome" : "Seremos História?",
+      "temporadas" : "[{\"temporadaT\":\"Assistir\"}]",
+      "tipo" : "FILME",
+      "trailer" : "n/a"
+    },
+    "Trolls 2" : {
+      "ano" : "2020",
+      "categoria" : "ANIMAÇÃO",
+      "descricao" : "A rainha Poppy e Branch fazem uma descoberta surpreendente: há outros mundos Troll além do seu, e suas diferenças criam grandes confrontos entre essas diversas tribos. Quando uma ameaça misteriosa coloca todos os Trolls do país em perigo, Poppy, Branch e seu grupo de amigos devem embarcar em uma jornada épica para criar harmonia entre os Trolls rivais e uní-los contra um mal maior.",
+      "episodios" : "[{\"temporadaE\":\"Assistir\",\"link\":\"https://tgfilestreamer.herokuapp.com/14979096148525236/@BluRayFilmes_Trolls%202%20(2020\",\"episodioE\":\"Dublado\"}]",
+      "filtro" : "todos",
+      "foto" : "https://firebasestorage.googleapis.com/v0/b/streamingfast-d7721.appspot.com/o/storage%2FIMG_20210204_141155_263.jpg?alt=media&token=69c56347-26d1-4eac-9b60-3051b9910551",
+      "imdb" : "6.0",
+      "nome" : "Trolls 2",
+      "temporadas" : "[{\"temporadaT\":\"Assistir\"}]",
+      "tipo" : "FILME",
+      "trailer" : "n/a"
+    },
+    "Um Caso de Detetive" : {
+      "ano" : "2020",
+      "categoria" : "COMÉDIA",
+      "descricao" : "Um detetive infantil que já foi famoso, agora adulto, continua resolvendo os mesmos mistérios triviais. Mas um dia, chega o seu primeiro caso adulto e precisa descobrir quem cometeu um brutal assassinato.",
+      "episodios" : "[{\"temporadaE\":\"Assistir\",\"link\":\"https://tgfilestreamer.herokuapp.com/14846192680517812/Um%20Caso%20de%20Detetive%20%5B2021%5D%20720p%20Web%20Dub%20@PFilmes.mp4\",\"episodioE\":\"Dublado\"}]",
+      "filtro" : "todos",
+      "foto" : "https://firebasestorage.googleapis.com/v0/b/streamingfast-d7721.appspot.com/o/storage%2FIMG_20210201_143225_897.jpg?alt=media&token=16e72400-57c1-4cbf-a88c-3dc44c491118",
+      "imdb" : "7.0",
+      "nome" : "Um Caso de Detetive",
+      "temporadas" : "[{\"temporadaT\":\"Assistir\"}]",
+      "tipo" : "FILME",
+      "trailer" : "n/a"
+    },
+    "Zona de Combate" : {
+      "ano" : "2021",
+      "categoria" : "AÇÃO",
+      "descricao" : "No futuro, Harp, piloto de drone, é enviado a uma violenta zona militarizada, onde precisa trabalhar para Leo, um oficial androide incumbido de encontrar um dispositivo de destruição em massa antes dos insurgentes.",
+      "episodios" : "[{\"temporadaE\":\"Assistir\",\"link\":\"https://tgfilestreamer.herokuapp.com/14974835540967604/@BluRayFilmes_Zona%20de%20Combate%20(2021\",\"episodioE\":\"DUBLADO\"}]",
+      "filtro" : "todos",
+      "foto" : "https://firebasestorage.googleapis.com/v0/b/streamingfast-d7721.appspot.com/o/storage%2FIMG_20210204_113735_057.jpg?alt=media&token=fa60a833-a7d8-4106-b29e-dc881349c9e8",
+      "imdb" : "6.0",
+      "nome" : "Zona de Combate",
+      "temporadas" : "[{\"temporadaT\":\"Assistir\"}]",
+      "tipo" : "FILME",
+      "trailer" : "n/a"
+    }
+  },
+  "Series" : {
+    "ksksk" : {
+      "categoriaSeries" : "film",
+      "descricao" : "ksksksk",
+      "episodios" : "[{\"temporadaE\":\"1\",\"link\":\"kkkkk\",\"episodioE\":\"kkk\"}]",
+      "foto" : "https://firebasestorage.googleapis.com/v0/b/streamingfast-d7721.appspot.com/o/logoSeries%2FScreenshot_20210227-155342516.jpg?alt=media&token=b48487a1-6248-4a0e-8b6b-1d496a5805cb",
+      "nome" : "ksksk",
+      "temporadas" : "[{\"temporadaT\":\"1\"}]"
+    }
+  },
+  "categorias" : {
+    "ANIMAÇÃO" : {
+      "categoriaSeries" : "ANIMAÇÃO"
+    },
+    "AVENTURA" : {
+      "categoriaSeries" : "AVENTURA"
+    },
+    "AÇÃO" : {
+      "categoriaSeries" : "AÇÃO"
+    },
+    "COMÉDIA" : {
+      "categoriaSeries" : "COMÉDIA"
+    },
+    "DOCUMENTÁRIO" : {
+      "categoriaSeries" : "DOCUMENTÁRIO"
+    },
+    "DRAMA" : {
+      "categoriaSeries" : "DRAMA"
+    },
+    "FAMÍLIA" : {
+      "categoriaSeries" : "FAMÍLIA"
+    },
+    "FANTASIA" : {
+      "categoriaSeries" : "FANTASIA"
+    },
+    "GUERR" : {
+      "categoriaSeries" : "GUERR"
+    },
+    "HISTÓRIA" : {
+      "categoriaSeries" : "HISTÓRIA"
+    },
+    "MISTÉRIO" : {
+      "categoriaSeries" : "MISTÉRIO"
+    },
+    "MUSICAL" : {
+      "categoriaSeries" : "MUSICAL"
+    },
+    "POLICIAL" : {
+      "categoriaSeries" : "POLICIAL"
+    },
+    "SUSPENSE" : {
+      "categoriaSeries" : "SUSPENSE"
+    },
+    "TERROR" : {
+      "categoriaSeries" : "TERROR"
     }
   }
 }
